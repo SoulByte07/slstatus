07383e88d5588a1d2bef9742af5be871297ae223
SoulByte07 <soul3147@proton.me>
2026-02-21 10:05:03 +0530

    config.def.h: + internet speed: color code

--- config.h
+++ config.h
@@ -68,7 +68,9 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
     /* function format                 argument */
 //    { disk_perc,    "\033[34;4mDisk: %s%%\033[0m | ", "/" },         // Blue disk usage
-    { ram_used,     "\033[31;4mRAM: %s%GB\033[0m | ", NULL },         // Red memory usage
+    { netspeed_rx,  "\033[34;4mUP: %s$\033[0m | ", NULL},
+    { netspeed_tx,  "\033[34;4mDOWN: %s$\033[0m | ", NULL},
+    { ram_used,     "\033[31;4mRAM: %s%\033[0m | ", NULL },         // Red memory usage
     { cpu_perc,     "\033[32;4mCPU: %s%%\033[0m | ", NULL },         // Green CPU usage
     { battery_perc, "\033[33;4mBat: %s%%\033[0m | ", "BAT0" },     // Yellow + underlined
     { datetime,     "\033[35;4m%s\033[0m", "%l:%M %p" },           // Purple + underlined time
